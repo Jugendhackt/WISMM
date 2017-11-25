fb93c22d5401ce4314e0a459a9dbcb5365f82872
Clemens <30525105+latoxgaming@users.noreply.github.com>
2017-11-25 15:22:35 +0100

    deafd

--- index.php
+++ index.php
@@ -24,21 +24,6 @@
                 padding-top: 56px;
             }
         }
-        #imaginary_container{
-                                  margin-top:20%; /* Don't copy this */
-                              }
-        .stylish-input-group .input-group-addon{
-            background: white !important;
-        }
-        .stylish-input-group .form-control{
-            border-right:0;
-            box-shadow:0 0 0;
-            border-color:#ccc;
-        }
-        .stylish-input-group button{
-            border:0;
-            background:transparent;
-        }
     </style>
 
 
@@ -67,25 +52,23 @@
     </div>
 </nav>
 
+<br/><br/><br/>
 <div class="jumbotron">
 
     <div class="container">
-
         <div class="row">
-            <div class="col-sm-6 col-sm-offset-3">
-                <div id="imaginary_container">
-                    <div class="input-group stylish-input-group">
-                        <input type="text" class="form-control"  placeholder="Search" >
-                        <span class="input-group-addon">
-                        <button type="submit">
-                            <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                    </span>
-                    </div>
+            <h2>Stylish Search Box</h2>
+            <div id="custom-search-input">
+                <div class="input-group col-md-12">
+                    <input type="text" class="  search-query form-control" placeholder="Search" />
+                    <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
                 </div>
             </div>
         </div>
-
     </div>
 
 </div>

2a5a179e1d13f344bf34a98b1a64a0be93273c32
kaschunuss <kaschunuss26@googlemail.com>
2017-11-25 14:43:20 +0100

    db & insert

---
+++ handler/insertH.php
@@ -0,0 +1,14 @@
+<?php
+
+	require "dbH.php";
+	
+	//db.connect("sql535.your-server.de", "krakennetz_w", "Z3WZS93EvQCDuZju", "krakennetz");
+
+	$network_name = $_GET['network_name'];
+	$network_url = $_GET['network_url'];
+
+
+	echo($network_name);
+	echo($network_url);
+
+?>

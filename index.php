08e4b0fcfa7e088ef2dbf96b4726b8e52fc42e6b
Clemens <30525105+latoxgaming@users.noreply.github.com>
2017-11-26 10:52:54 +0100

    added some magic comments over the search bar

--- index.php
+++ index.php
@@ -61,10 +61,86 @@
 
     <div class="starter-template">
 
+        <script>
+            //made by vipul mirajkar thevipulm.appspot.com
+            var TxtType = function(el, toRotate, period) {
+                this.toRotate = toRotate;
+                this.el = el;
+                this.loopNum = 0;
+                this.period = parseInt(period, 10) ||1500;
+                this.txt = '';
+                this.tick();
+                this.isDeleting = false;
+            };
+
+            TxtType.prototype.tick = function() {
+                var i = this.loopNum % this.toRotate.length;
+                var fullTxt = this.toRotate[i];
+
+                if (this.isDeleting) {
+                    this.txt = fullTxt.substring(0, this.txt.length - 1);
+                } else {
+                    this.txt = fullTxt.substring(0, this.txt.length + 1);
+                }
+
+                this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+                var that = this;
+                var delta = 200 - Math.random() * 100;
+
+                if (this.isDeleting) { delta /= 2; }
+
+                if (!this.isDeleting && this.txt === fullTxt) {
+                    delta = this.period;
+                    this.isDeleting = true;
+                } else if (this.isDeleting && this.txt === '') {
+                    this.isDeleting = false;
+                    this.loopNum++;
+                    delta = 500;
+                }
+
+                setTimeout(function() {
+                    that.tick();
+                }, delta);
+            };
+
+            window.onload = function() {
+                var elements = document.getElementsByClassName('typewrite');
+                for (var i=0; i<elements.length; i++) {
+                    var toRotate = elements[i].getAttribute('data-type');
+                    var period = elements[i].getAttribute('data-period');
+                    if (toRotate) {
+                        new TxtType(elements[i], JSON.parse(toRotate), period);
+                    }
+                }
+                // INJECT CSS
+                var css = document.createElement("style");
+                css.type = "text/css";
+                css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+                document.body.appendChild(css);
+            };
+        </script>
+
+        <style>
+            /* mouse over link */
+            a:hover {
+                color: dodgerblue;
+                text-decoration: none;
+            }
+            a {
+                color: dodgerblue;
+                text-decoration: none;
+            }
+
+        </style>
+
 
         <img src="https://github.com/Jugendhackt/KrakenNetz/blob/master/images/Lupe_2.png?raw=true" class="img.responsive" width="15%" alt="Lupe">
-
-        <h2>Stay transparent - Stay safe - Stay alpaca!</h2>
+        <h2>
+            <a class="typewrite" data-period="2000" data-type='[ "Stay Transparent.", "Stay Safe.", "Stay Alpaca.", "Love Jugend hackt <3" ]'>
+                <span class="wrap"></span>
+            </a>
+        </h2>
         <div class="row">
             <div id="custom-search-input">
                 <div class="input-group col-md-12">

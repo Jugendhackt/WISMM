7b5df70666679acfad8c161791ff46fa7a329791
Clemens <30525105+latoxgaming@users.noreply.github.com>
2017-11-29 00:14:09 +0100

    changed css file

--- index.php
+++ index.php
@@ -404,7 +404,6 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="https://getbootstrap.com/docs/3.3/dist/js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function(){
